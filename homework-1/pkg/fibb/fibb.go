85e18282f776dfa5901b5c9191e4b2222dcddad5
Vlad Filiucov <filiucov@mail.ru>
2020-10-15 23:35:56 +0100

    don't throw an exception

--- homework-1/pkg/fibb/fibb.go
+++ homework-1/pkg/fibb/fibb.go
@@ -1,17 +1,14 @@
 package fibb
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func Fib(n int) int {
 	if n > 20 {
 		fmt.Println("We don't support numbers higher than 20")
-		os.Exit(1)
+		return -1
 	}
 
-	var a, b int = 1, 1
+	var a, b int = 0, 1
 	for i := 0; i < n; i++ {
 		a, b = b, a+b
 	}
